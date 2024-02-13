8dc5101a5dae1f466c9b391f3631392356ad0e0b
Spastic <junior.tan@live.com>
2024-02-13 16:36:12 +0800

    Revert "start tanstack router migration"
    This reverts commit 1690ed75773d04d5741da5e5a8a3279a0d16a7b0.

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -1,35 +1,13 @@
-import {StrictMode} from 'react'
-import ReactDOM from 'react-dom/client'
-import './index.css'
-import { AuthContextProvider } from './contexts/AuthContext.jsx'
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
-import { RouterProvider, createRouter } from '@tanstack/react-router'
+import AppRouter from "./routes/AppRouter";
 
-// Import the generated route tree
-import { routeTree } from './routeTree.gen.ts'
+function App() {
 
-// Create a new router instance
-const router = createRouter({ routeTree })
-const queryClient = new QueryClient()
+  return (
+    <div className="min-h-screen">
+      <AppRouter />
+    </div>
+  );
 
-// // Register the router instance for type safety
-// declare module '@tanstack/react-router' {
-//   interface Register {
-//     router: typeof router
-//   }
-// }
-
-// Render the app
-const rootElement = document.getElementById('app')
-if (!rootElement.innerHTML) {
-  const root = ReactDOM.createRoot(rootElement)
-  root.render(
-    <StrictMode>
-      <QueryClientProvider client={queryClient}>
-        <AuthContextProvider>
-        <RouterProvider router={router} />
-        </AuthContextProvider>
-      </QueryClientProvider>
-    </StrictMode>
-  )
 }
+
+export default App
---
+++ front-end/src/main.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
+import { AuthContextProvider } from './contexts/AuthContext.jsx'
+import {
+  QueryClient,
+  QueryClientProvider
+} from '@tanstack/react-query'
+
+
+
+const queryClient = new QueryClient()
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <QueryClientProvider client={queryClient}>
+      <AuthContextProvider>
+        <App />
+      </AuthContextProvider>
+    </QueryClientProvider>
+  </React.StrictMode>,
+)
