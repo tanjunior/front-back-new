ae7308a88794f6fdc2802ccb6c912fc7591da374
Spastic <junior.tan@live.com>
2024-02-14 15:36:14 +0800

    cart context

--- front-end/src/main.jsx
+++ front-end/src/main.jsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import { AuthContextProvider } from './contexts/AuthContext.jsx'
+import { CartContextProvider } from './contexts/CartContext.jsx'
 import {
   QueryClient,
   QueryClientProvider
@@ -17,7 +18,10 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
       <AuthContextProvider>
-        <App />
+        <CartContextProvider>
+          <App />
+          <Toaster />
+        </CartContextProvider>
       </AuthContextProvider>
     </QueryClientProvider>
   </React.StrictMode>,
