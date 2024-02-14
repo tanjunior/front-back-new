8d4d93b361e95cc95bf30877399742cea33054ca
Spastic <junior.tan@live.com>
2024-02-14 15:38:47 +0800

    product add to cart, toast success

--- front-end/src/pages/HomePage.jsx
+++ front-end/src/pages/HomePage.jsx
@@ -1,5 +1,4 @@
-import { useQuery } from '@tanstack/react-query'
-import { Button } from '../components/ui/button'
+import { useMutation, useQuery } from '@tanstack/react-query'
 import Icons from '../components/ui/Icons'
 import { Link } from 'react-router-dom'
 import {
@@ -9,16 +8,14 @@ import {
   CardHeader,
 } from "@/components/ui/card"
 import { Input } from '@/components/ui/input'
-import {
-  Select,
-  SelectContent,
-  SelectItem,
-  SelectTrigger,
-  SelectValue,
-} from "@/components/ui/select"
-
+import { toast } from "sonner"
+import useAuth from '@/hooks/useAuth'
+import useCart from '@/hooks/useCart'
 
 export default function HomePage() {
+  const { user } = useAuth()
+  const { cart, setCart } = useCart()
+
   const { data: products, isError, isLoading } = useQuery({
     queryKey: ['products'],
     queryFn: async () => {
@@ -28,55 +25,64 @@ export default function HomePage() {
     },
   })
 
+  const {mutate} = useMutation({
+    mutationFn: async (data) => {
+      return fetch(`http://localhost:3001/api/carts/add`, {
+        method: "POST",
+        body: JSON.stringify({...data, shoppingCartId: user.shoppingCart.id}),
+        headers: {
+          "Content-Type": "application/json",
+        }
+      }).then(async(res) => await res.json())
+    }
+  })
+
   return (
-    <div className='flex flex-row items-end justify-between w-full h-full gap-4 mt-28'>
-      <div className='w-2/5'>filter</div>
-      <div className='flex flex-col w-full gap-y-12'>
-        <div className='flex flex-row justify-between gap-x-32'>
-          <div className='flex flex-row w-full gap-x-2'>
-            <Input type="text" className='rounded-md border border-[#E4E7E9] w-full max-w-80'/>
-            <Select>
-              <SelectTrigger className="w-[180px]">
-                <span  className='text-secondary'>asd</span>
-                <SelectValue placeholder="??" />
-              </SelectTrigger>
-              <SelectContent>
-                <SelectItem value="light">Light</SelectItem>
-                <SelectItem value="dark">Dark</SelectItem>
-                <SelectItem value="system">System</SelectItem>
-              </SelectContent>
-            </Select>
-          </div>
-          <div>8/400</div>
-        </div>
+    <div className='flex flex-col items-center justify-center w-full h-full gap-4 gap-y-12 mt-28'>
+      <Input type="text" className='rounded-md border border-[#E4E7E9] w-full'/>
 
-        <div className='grid grid-cols-4 gap-2'>
-          {
-            isLoading ? <div>Loading...</div> : (products.length > 0) ? products.map((product) => (
+      <div className='grid grid-cols-3 gap-8 px-44'>
+        {
+          isLoading ? <div>Loading...</div> : (products.length > 0) ? products.map((product) => (
+            
+
+            <Link to={`/product/${product.id}`} key={product.id}>
+              <Card>
+                <CardHeader>
+                  <img src={`http://localhost:3001/images/${product.productImg}`} alt="" className='object-scale-down aspect-square' />
+                </CardHeader>
+                <CardContent>
+                <div>{product.name} ({product.capacity}) - {product.color}</div>
+                <div className='pl-2 text-primary'>{product.price}</div>
+                </CardContent>
+                <CardFooter className="flex justify-end">
+                  <Icons.cart className="z-10 w-10 h-10" onClick={(e)=> {
+                    e.preventDefault()
+                    e.stopPropagation()
+                    mutate({ productId: product.id, quantity: 1 }, {
+                      onSuccess: (data) => {
+                        setCart(prev => prev.map((item) => {
+                          if (item.product.id === product.id) {item.quantity = data.quantity} return item
+                        }))
+                        toast(`${product.name} was added to cart`, {
+                          action: {
+                            label: 'Undo',
+                            onClick: () => console.log('Undo')
+                          }
+                        })
+                      }
+                    })
+                  }
+                  }/>
+                </CardFooter>
+              </Card>
               
-
-              <Link to={`/product/${product.id}`} key={product.id}>
-                <Card>
-                  <CardHeader>
-                    <img src={`http://localhost:3001/images/${product.productImg}`} alt="" />
-                  </CardHeader>
-                  <CardContent>
-                  <div>{product.name} ({product.capacity}) - {product.color}</div>
-                  <div className='pl-2 text-primary'>{product.price}</div>
-                  </CardContent>
-                  <CardFooter className="flex justify-end">
-                    <Icons.cart className="w-10 h-10"/>
-                  </CardFooter>
-                </Card>
-                
-                
-                
-              </Link>
-            )) : <div>No products found</div>
-          }
-        </div>
+              
+              
+            </Link>
+          )) : <div>No products found</div>
+        }
       </div>
-
     </div>
   )
 }
