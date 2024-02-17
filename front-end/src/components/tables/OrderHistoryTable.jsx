5950e3741e0a1925944bc6229e225027f68be0a5
Spastic <junior.tan@live.com>
2024-02-17 14:50:19 +0800

    use reusable table component

--- front-end/src/components/tables/OrderHistoryTable.jsx
+++ front-end/src/components/tables/OrderHistoryTable.jsx
@@ -1,122 +1,29 @@
-/* eslint-disable react/prop-types */
-import { useState } from "react"
-import {
-  flexRender,
-  getCoreRowModel,
-  getFilteredRowModel,
-  getPaginationRowModel,
-  getSortedRowModel,
-  useReactTable,
-} from "@tanstack/react-table"
-import {
-  Table,
-  TableBody,
-  TableCell,
-  TableHead,
-  TableHeader,
-  TableRow,
-} from "@/components/ui/table"
-import {
-  Pagination,
-  PaginationContent,
-  PaginationEllipsis,
-  PaginationItem,
-  PaginationLink,
-  PaginationNext,
-  PaginationPrevious,
-} from "@/components/ui/pagination"
+import { OrderHistoryColumns } from './OrderHistoryColumns'
+import useAuth from '@/hooks/useAuth'
+import { useQuery } from '@tanstack/react-query'
+import { DataTable } from './DataTable'
 
-export function OrderHistoryTable({ columns, data }) {
-  const [sorting, setSorting] = useState([])
-  const [columnFilters, setColumnFilters] = useState([])
-  const [columnVisibility, setColumnVisibility] = useState({})
-  const [rowSelection, setRowSelection] = useState({})
-
-  const table = useReactTable({
-    data,
-    columns,
-    getCoreRowModel: getCoreRowModel(),
-    getPaginationRowModel: getPaginationRowModel(),
-    onSortingChange: setSorting,
-    getSortedRowModel: getSortedRowModel(),
-    onColumnFiltersChange: setColumnFilters,
-    getFilteredRowModel: getFilteredRowModel(),
-    onColumnVisibilityChange: setColumnVisibility,
-    onRowSelectionChange: setRowSelection,
-    state: {
-      sorting,
-      columnFilters,
-      columnVisibility,
-      rowSelection,
+export default function UserOrders() {
+  const { user } = useAuth()
+  const { data: products, isError, isLoading } = useQuery({
+    queryKey: ['products'],
+    queryFn: async () => {
+      const response = await fetch(`http://localhost:3001/api/orders/user/${user.id}`)
+      const data = await response.json()
+      // console.log(data)
+      return data
     },
   })
 
+  if (isError) return <div>เกิดข้อผิดพลาด</div>
+  if (isLoading) return <div>กำลังโหลด...</div>
+
   return (
-    <div>
-      <div className="border rounded-md">
-        <Table>
-          <TableHeader>
-            {table.getHeaderGroups().map((headerGroup) => (
-              <TableRow key={headerGroup.id}>
-                {headerGroup.headers.map((header) => {
-                  return (
-                    <TableHead key={header.id}>
-                      {header.isPlaceholder
-                        ? null
-                        : flexRender(
-                            header.column.columnDef.header,
-                            header.getContext()
-                          )}
-                    </TableHead>
-                  )
-                })}
-              </TableRow>
-            ))}
-          </TableHeader>
-          <TableBody>
-            {table.getRowModel().rows?.length ? (
-              table.getRowModel().rows.map((row) => (
-                <TableRow
-                  key={row.id}
-                  data-state={row.getIsSelected() && "selected"}
-                >
-                  {row.getVisibleCells().map((cell) => (
-                    <TableCell key={cell.id}>
-                      <div className="flex flex-row items-center">
-                        {
-                          flexRender(cell.column.columnDef.cell, cell.getContext())
-                        }
-                      </div>
-                    </TableCell>
-                  ))}
-                </TableRow>
-              ))
-            ) : (
-              <TableRow>
-                <TableCell colSpan={columns.length} className="h-24 text-center">
-                  ไม่พบผลลัพท์
-                </TableCell>
-              </TableRow>
-            )}
-          </TableBody>
-        </Table>
-      </div>
-      <Pagination className="flex items-center justify-center">
-        <PaginationContent>
-          <PaginationItem>
-            <PaginationPrevious href="#" />
-          </PaginationItem>
-          <PaginationItem>
-            <PaginationLink href="#">1</PaginationLink>
-          </PaginationItem>
-          <PaginationItem>
-            <PaginationEllipsis />
-          </PaginationItem>
-          <PaginationItem>
-            <PaginationNext href="#" />
-          </PaginationItem>
-        </PaginationContent>
-      </Pagination>
+    <div className='flex flex-col flex-1'>
+      <h1>คำสั่งซื้อ</h1>
+      
+      <DataTable columns={OrderHistoryColumns} data={products}/>
+
     </div>
   )
 }
