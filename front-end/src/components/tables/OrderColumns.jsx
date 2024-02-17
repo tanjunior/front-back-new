9e8f3bae8f450292b694a7a7b80b815e942810d8
Spastic <junior.tan@live.com>
2024-02-17 17:33:39 +0800

    Update OrderColumns.jsx

--- front-end/src/components/tables/OrderColumns.jsx
+++ front-end/src/components/tables/OrderColumns.jsx
@@ -1,4 +1,5 @@
 import { Link } from "react-router-dom"
+import { Button } from "@/components/ui/button"
 
 export const OrderColumns = [
   {
@@ -19,53 +20,28 @@ export const OrderColumns = [
     cell: ({ row }) => {
       // const total = 0
       const total = row.original?.orderDetails?.reduce((acc, cur) => acc + cur.price * cur.quantity, 0)
-      console.log(row.original)
       const amount = parseFloat(total)
       const formatted = new Intl.NumberFormat("en-US", {
         style: "currency",
         currency: "THB",
       }).format(amount)
-      return <div className="font-medium text-right">{formatted}</div>
+      return <div className="font-medium">{formatted}</div>
     },
   },
   {
     id: "actions",
     cell: ({ row }) => {
-      const productId = row.original.id
-      const queryClient = useQueryClient() // can't invalidate because I can't use a hook in here
+      const orderId = row.original.id
 
       return (
-        <DropdownMenu>
-          <DropdownMenuTrigger asChild>
-            <Button variant="ghost" className="w-8 h-8 p-0">
-              <span className="sr-only">Open menu</span>
-              <MoreHorizontal className="w-4 h-4" />
-            </Button>
-          </DropdownMenuTrigger>
-          <DropdownMenuContent align="end">
-            <Link 
-              to={`/products/${productId}`}
-              state={row.original}
-            >
-              <DropdownMenuItem>
-              Edit
-              </DropdownMenuItem>
-            </Link>
-            <DropdownMenuSeparator />
-            <DropdownMenuItem
-              className="text-destructive"
-              onClick={() => axios.delete(
-                "http://localhost:3001/api/products/delete",
-                {data: {id: productId}}
-              ).then(res => {
-                if (res.status === 200) {
-                  toast("deleted")
-                  queryClient.invalidateQueries("products")
-                }
-              })}
-            >Delete</DropdownMenuItem>
-          </DropdownMenuContent>
-        </DropdownMenu>
+        <Button asChild>
+          <Link 
+            to={`/orders/${orderId}`}
+            state={row.original}
+          >
+            Details
+          </Link>
+        </Button>
       )
     },
   },
