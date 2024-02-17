ba54ea0f0e58049cab0ebca0ed4a0f50c14d076c
Spastic <junior.tan@live.com>
2024-02-17 21:42:29 +0800

    Update OrderTable.jsx

--- front-end/src/components/tables/OrderTable.jsx
+++ front-end/src/components/tables/OrderTable.jsx
@@ -1,9 +1,11 @@
 import { OrderColumns } from './OrderColumns'
 import { useQuery } from '@tanstack/react-query'
 import { DataTable } from './DataTable'
-import { NavLink } from 'react-router-dom'
+import { NavLink, useLocation } from 'react-router-dom'
 
 export default function OrderTable() {
+  const {pathname} = useLocation()
+  console.log(pathname)
   const { data: products, isError, isLoading } = useQuery({
     queryKey: ['products'],
     queryFn: async () => {
@@ -19,13 +21,15 @@ export default function OrderTable() {
 
   return (
     <div className="flex flex-col">
-      <div className="flex flex-row items-start justify-between">
-        <div>
-          <NavLink to="/">Dashboard</NavLink>
-          {" > "}
-          <span className="text-primary">Order List</span>
+      { pathname !== "/" && (
+        <div className="flex flex-row items-start justify-between">
+          <div>
+            <NavLink to="/">Dashboard</NavLink>
+            {" > "}
+            <span className="text-primary">Order List</span>
+          </div>
         </div>
-      </div>
+      )}
       <DataTable columns={OrderColumns} data={products}/>
     </div>
   )
